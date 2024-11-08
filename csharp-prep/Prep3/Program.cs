d6b1c59873b13e6a5561d7c5745ef02f8a2f03e7
DMStanle <sta18060@byui.edu>
2024-11-08 00:18:44 -0700

    Completed C# practice 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+     {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+        int guess;
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+        } while (guess != magicNumber);
+
+        Console.WriteLine("You guessed it!");
     }
+
 }
+

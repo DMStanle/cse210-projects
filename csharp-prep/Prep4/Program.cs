87b47caf9c69d07163d51fe4fb60349176713aae
DMStanle <sta18060@byui.edu>
2024-11-08 20:38:16 -0700

    Complettion of the project.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,33 @@
-using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        string userNumber;
+        int number;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (true)
+        {
+            Console.Write("Enter a number: ");
+            userNumber  = Console.ReadLine();
+            if (int.TryParse(userNumber, out number))
+            {
+                if (number == 0)
+                {
+                    break;
+                }
+                numbers.Add(number);
+
+            }
+        }
+        int sum = numbers.Sum();
+        Console.WriteLine($"The sum is: {sum}");
+        double average = numbers.Average();
+        Console.WriteLine($"The average is: {average}");
+        int highestNumber = numbers.Max();
+        Console.WriteLine($"The largest number is: {highestNumber}");
+
     }
 }

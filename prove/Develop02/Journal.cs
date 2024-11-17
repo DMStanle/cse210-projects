e16d2715044465159b71860f8e776a9226943f09
DMStanle <sta18060@byui.edu>
2024-11-16 19:37:00 -0700

    assignment still in progress

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,13 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
 public class Journal
 {
     public List<Entry> _entries;
+
+    public Journal()
+    {
+        _entries = new List<Entry>();
+    }
     
     public void AddEntry(Entry newEntry)
     {
+        _entries.Add(newEntry);
 
-    }
+
+    }    
     public void DisplayAll()
     {
+        foreach (var entry in _entries)
+        {
+            entry.Display();
+            Console.WriteLine();
+        }
 
     }
 
